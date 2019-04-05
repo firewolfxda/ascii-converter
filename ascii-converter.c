2b7f986126160edffe560919a5c9a8b771b8c895
firewolfxda <firewolfxda@outlook.com>
2019-04-05 15:25:04 +0600

    proper marks

--- ascii-converter.c
+++ ascii-converter.c
@@ -7,7 +7,7 @@ int ansiiconv ()
         char input;
         printf("\n\tPlease enter a ANSII character:\n\t");
         scanf("%c",&input);
-        printf("\tThe decimal value of \"%c\" is \"%d\"\n",input, input);
+        printf("\tThe decimal value of \"%c\" is \"%d\".\n",input, input);
         fflush(stdin);
         return 0;
 }
@@ -16,7 +16,7 @@ int main(){
         char looper;
         do {
                 ansiiconv ();
-                printf("\n\tConvert another ANSII character?:\n\tPress y for yes, any key to exit\n\t");
+                printf("\n\tConvert another ANSII character?\n\tPress y for yes, any key to exit\n\t");
                 scanf("%c", &looper);
                 fflush(stdin);
         }
